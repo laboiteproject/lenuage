0bd33f7da2795e330d4fa902b6815a3829b2289c
Francois Lemercier <lemercier.francois@gmail.com>
2016-10-14 17:41:51 +0200

    corrections

--- app_alarm/admin.py
+++ app_alarm/admin.py
@@ -3,8 +3,6 @@ from django.contrib import admin
 from .models import AppAlarm
 
 class AppAlarmAdmin(admin.ModelAdmin):
-    readonly_fields = ()
-
-    list_display = ('heure','minutes',)
+    list_display = ('heure', 'minutes')
 
 admin.site.register(AppAlarm, AppAlarmAdmin)
