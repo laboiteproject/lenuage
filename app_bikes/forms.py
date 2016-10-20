64af079f1db04a238d241b53a227f342e65dd87a
Vincent Rialland <vincent.rialland@gmail.com>
2016-10-20 12:31:32 +0200

    Display saved station

--- app_bikes/forms.py
+++ app_bikes/forms.py
@@ -3,6 +3,12 @@ from django import forms
 
 
 class BikeModelForm(forms.ModelForm):
+    def __init__(self, *args, **kw):
+        super(BikeModelForm, self).__init__(*args, **kw)
+        if self.instance is not None:
+            # Setup choices to display selected station
+            self.fields['id_station'].widget.choices = ((self.instance.id_station, self.instance.station),)
+
     class Meta:
         widgets = {
             'id_station': autocomplete.ListSelect2(url='station-autocomplete', forward=('provider',))
