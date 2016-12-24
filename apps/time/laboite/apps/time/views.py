c76c6c2af8f2d17b634b4668a6f0c74844d14206
Vincent Rialland <vincent.rialland@gmail.com>
2016-12-24 15:20:07 +0100

    More generic views

--- apps/time/laboite/apps/time/views.py
+++ apps/time/laboite/apps/time/views.py
@@ -1,59 +1,18 @@
 # coding: utf-8
 from __future__ import unicode_literals
-from django.shortcuts import get_object_or_404
-from django.views.generic.edit import UpdateView, CreateView, DeleteView
-from django.contrib.messages.views import SuccessMessageMixin
-from django.contrib import messages
-from django.utils.translation import ugettext_lazy as _
-from django.urls import reverse_lazy
-
 from .models import AppTime
-from boites.models import Boite
+from boites.views import AppCreateView, AppUpdateView, AppDeleteView
 
 
-class AppTimeUpdateView(UpdateView):
+class AppTimeCreateView(AppCreateView):
+    model = AppTime
+    fields = ['tz']
+
+
+class AppTimeUpdateView(AppUpdateView):
     model = AppTime
     fields = ['tz', 'enabled']
 
-    def get_context_data(self, **kwargs):
-        context = super(AppTimeUpdateView, self).get_context_data(**kwargs)
-        verbose_name = self.object._meta.verbose_name.title()
-        context['verbose_name'] = verbose_name
-        context['boite_id'] = self.kwargs.get('boite_pk')
-        return context
 
-    def get_success_url(self):
-        return reverse_lazy('boites:update', kwargs={'pk': self.kwargs.get('boite_pk')})
-
-
-class AppTimeCreateView(SuccessMessageMixin, CreateView):
+class AppTimeDeleteView(AppDeleteView):
     model = AppTime
-    fields = ['tz']
-    success_message = _('App a bien été créée !')
-
-    def get_context_data(self, **kwargs):
-        context = super(AppTimeCreateView, self).get_context_data(**kwargs)
-        context['boite_id'] = self.kwargs.get('boite_pk')
-        return context
-
-    def get_success_url(self):
-        return reverse_lazy('boites:update', kwargs={'pk': self.kwargs.get('boite_pk')})
-
-    def form_valid(self, form):
-        boite = get_object_or_404(Boite, pk=self.kwargs.get('boite_pk'), user=self.request.user)
-        form.instance.boite = boite
-        form.save()
-        return super(AppTimeCreateView, self).form_valid(form)
-
-
-class AppTimeDeleteView(DeleteView):
-    model = AppTime
-
-    def get_context_data(self, **kwargs):
-        context = super(AppTimeDeleteView, self).get_context_data(**kwargs)
-        context['boite_id'] = self.kwargs.get('boite_pk')
-        return context
-
-    def get_success_url(self):
-        messages.error(self.request, _('App supprimée !'))
-        return reverse_lazy('boites:update', kwargs={'pk': self.kwargs.get('boite_pk')})
