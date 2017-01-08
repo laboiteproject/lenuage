959e81086e0a44771273fc85b98d5432300f3f6f
Baptiste Gaultier <b.gaultier@gmail.com>
2017-01-08 15:09:24 +0100

    AppWeather views fixed

--- apps/weather/laboite/apps/weather/urls.py
+++ apps/weather/laboite/apps/weather/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib.auth.decorators import login_required
 
-from .views import AppWeatherUpdateView
+from .views import AppWeatherUpdateView, AppWeatherCreateView, AppWeatherDeleteView
 
 urlpatterns = [
+    url(r"^create/$", login_required(AppWeatherCreateView.as_view()), name="create"),
     url(r"^(?P<pk>\d+)/$", login_required(AppWeatherUpdateView.as_view()), name="update"),
+    url(r"^(?P<pk>\d+)/delete/$", login_required(AppWeatherDeleteView.as_view()), name="delete"),
 ]
--- apps/weather/laboite/apps/weather/views.py
+++ apps/weather/laboite/apps/weather/views.py
@@ -1,18 +1,64 @@
-from django.shortcuts import render
-from django.views.generic.edit import UpdateView
+# coding: utf-8
+
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
+from django.shortcuts import get_object_or_404
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
+from django.urls import reverse_lazy
+from django.http import JsonResponse
 
 from .models import AppWeather
+from boites.models import Boite
 
-class AppWeatherUpdateView(UpdateView):
+class AppWeatherUpdateView(SuccessMessageMixin, UpdateView):
     model = AppWeather
     fields = ['city_name', 'enabled']
 
-    success_url = '../../'
+    success_message = _(u"App modifiée avec succès !")
 
     def get_context_data(self, **kwargs):
         context = super(AppWeatherUpdateView, self).get_context_data(**kwargs)
         verbose_name = self.object._meta.verbose_name.title()
         context['verbose_name'] = verbose_name
         context['boite_id'] = self.kwargs.get('boite_pk')
+        context['asana_personal_access_token'] = self.object.asana_personal_access_token
 
         return context
+
+    def get_success_url(self):
+        return reverse_lazy('boites:update', kwargs={'pk': self.kwargs.get('boite_pk')})
+
+class AppWeatherCreateView(SuccessMessageMixin, CreateView):
+    model = AppWeather
+    fields = ['city_name']
+
+    success_message = _(u"App a bien été créée !")
+
+    def get_context_data(self, **kwargs):
+        context = super(AppWeatherCreateView, self).get_context_data(**kwargs)
+        context['boite_id'] = self.kwargs.get('boite_pk')
+
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('boites:update', kwargs={'pk': self.kwargs.get('boite_pk')})
+
+    def form_valid(self, form):
+        boite = get_object_or_404(Boite, pk=self.kwargs.get('boite_pk'), user=self.request.user)
+        form.instance.boite = boite
+        form.save()
+        return super(AppWeatherCreateView, self).form_valid(form)
+
+class AppWeatherDeleteView(DeleteView):
+    model = AppWeather
+
+    def get_context_data(self, **kwargs):
+        context = super(AppWeatherDeleteView, self).get_context_data(**kwargs)
+        context['boite_id'] = self.kwargs.get('boite_pk')
+
+        return context
+
+    def get_success_url(self):
+        messages.error(self.request, _(u"App supprimée !"))
+        return reverse_lazy('boites:update', kwargs={'pk': self.kwargs.get('boite_pk')})
