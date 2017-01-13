e4bdfa7b10c97bf91de259c81b785fc3baf0ae85
Vincent Rialland <vincent.rialland@gmail.com>
2017-01-13 21:57:54 +0100

    Improve calendar app

--- apps/calendar/laboite/apps/calendar/models.py
+++ apps/calendar/laboite/apps/calendar/models.py
@@ -1,62 +1,57 @@
 # coding: utf-8
 
 from __future__ import unicode_literals
+from datetime import datetime
 
 from django.db import models
-from django.utils import timezone
+from django.utils import formats, timezone
 from django.utils.translation import ugettext_lazy as _
-
-from boites.models import Boite, App
-from . import settings
-
-from icalendar import Calendar, Event
-from datetime import datetime, timedelta
-import requests
-from account.conf import settings
+from icalendar import Calendar
 import pytz
+import requests
 
-from . import settings
+from boites.models import App
+
 
 class AppCalendar(App):
-    ics_url = models.CharField(_(u"URL du calendrier ics"), help_text=_(u"Veuillez indiquer l'adresse de votre calendrier .ics"), max_length=256, default=None, null=True)
+    UPDATE_INTERVAL = 60 * 60
 
-    dtstart = models.CharField(_(u"Heure du prochain rendez-vous"), max_length=5, default=None, null=True)
-    summary = models.CharField(_(u"Intitulé du prochain rendez-vous"), max_length=256, default=None, null=True)
+    ics_url = models.CharField(_('URL du calendrier .ics'), help_text=_("Veuillez indiquer l'adresse de votre calendrier .ics"), max_length=256, default=None, null=True)
+    dtstart = models.CharField(_('Heure du prochain rendez-vous'), max_length=5, default=None, null=True)
+    summary = models.CharField(_('Intitulé du prochain rendez-vous'), max_length=256, default=None, null=True)
 
-    def get_app_dictionary(self):
-        if self.enabled:
-            # we wan't to update every VALUES_UPDATE_INTERVAL minutes
-            now = timezone.now()
-            if now <= self.created_date + timedelta(seconds=10) or now >= self.last_activity + timedelta(minutes=settings.VALUES_UPDATE_INTERVAL):
-                now = timezone.now()
-                today = now.replace(hour = 0, minute=0)
-                tonight = now.replace(hour = 23, minute=59)
+    def update_data(self):
+        # TODO: handle whole day events, display multiple events
+        now = timezone.now()
+        tonight = now.replace(hour=23, minute=59)
 
-                self.dtstart = None
-                self.summary = None
-                self.save()
+        self.dtstart = None
+        self.summary = None
 
-                calendar = Calendar()
-                r = requests.get(self.ics_url)
+        calendar = Calendar()
+        r = requests.get(self.ics_url)
 
-                calendar = calendar.from_ical(r.text.encode('ascii', 'replace'))
+        calendar = calendar.from_ical(r.text.encode('ascii', 'replace'))
 
-                for event in calendar.walk():
-                    # the following code is just bad
-                    #TODO need improvements
-                    if event.get('dtstart'):
-                        dtstart = event.get('dtstart').dt
-                        if type(dtstart) is not type(now):
-                            # crapy conversion
-                            dtstart = datetime.fromordinal(dtstart.toordinal())
-                        dtstart = dtstart.replace(tzinfo = pytz.timezone('UTC'))
-                        if dtstart > today and dtstart > now and dtstart < tonight:
-                            self.dtstart = dtstart.astimezone(pytz.timezone('Europe/Paris')).strftime("%H:%M")
-                            self.summary = str(event.get('summary'))
-                            self.save()
+        for event in calendar.walk():
+            if event.get('dtstart'):
+                dtstart = event.get('dtstart').dt
+                if not isinstance(dtstart, datetime):
+                    dtstart = datetime.fromordinal(dtstart.toordinal())
+                try:
+                    dtstart = timezone.localtime(dtstart, timezone.utc)
+                except ValueError:
+                    dtstart = dtstart.replace(tzinfo=timezone.utc)
+                if now <= dtstart <= tonight:
+                    dtstart = timezone.localtime(dtstart, pytz.timezone('Europe/Paris'))
+                    self.dtstart = formats.time_format(dtstart, 'TIME_FORMAT')
+                    self.summary = str(event.get('summary'))
+        self.save()
 
-            return {'dtstart': self.dtstart, 'summary' : self.summary}
+    def _get_data(self):
+        return {'dtstart': self.dtstart,
+                'summary': self.summary}
 
     class Meta:
-        verbose_name = _("Configuration : calendrier")
-        verbose_name_plural = _("Configurations : calendrier")
+        verbose_name = _('Configuration : calendrier')
+        verbose_name_plural = _('Configurations : calendrier')
