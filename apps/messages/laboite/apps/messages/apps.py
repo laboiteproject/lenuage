c5f5e27d8b0183923651eebcd1134b1acfd968b2
Vincent Rialland <vincent.rialland@gmail.com>
2017-01-14 17:39:54 +0100

    Improved messages app

--- apps/messages/laboite/apps/messages/apps.py
+++ apps/messages/laboite/apps/messages/apps.py
@@ -1,8 +1,9 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class AppMessagesConfig(AppConfig):
     name = label = 'laboite.apps.messages'
-    verbose_name = 'App : Message'
+    verbose_name = _('App : Message')
