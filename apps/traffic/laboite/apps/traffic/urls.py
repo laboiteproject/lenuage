a29d1458772d7e4f1cca28f47f61fd54d8571e21
Vincent Rialland <vincent.rialland@gmail.com>
2017-01-29 21:28:11 +0100

    Improved traffic app

--- apps/traffic/laboite/apps/traffic/urls.py
+++ apps/traffic/laboite/apps/traffic/urls.py
@@ -1,8 +1,11 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
-from .views import AppTrafficUpdateView
+from .views import AppTrafficCreateView, AppTrafficUpdateView, AppTrafficDeleteView
+
 
 urlpatterns = [
+    url(r"^create/$", login_required(AppTrafficCreateView.as_view()), name="create"),
     url(r"^(?P<pk>\d+)/$", login_required(AppTrafficUpdateView.as_view()), name="update"),
+    url(r"^(?P<pk>\d+)/delete/$", login_required(AppTrafficDeleteView.as_view()), name="delete"),
 ]
--- boites/exceptions.py
+++ boites/exceptions.py
@@ -1,2 +1,6 @@
 class ExternalWebserviceError(Exception):
     pass
+
+
+class ExternalDataError(Exception):
+    pass
