bb8990772040d6d8e05f22e4a04897247e4f1ed2
Vincent Rialland <vincent.rialland@gmail.com>
2017-01-31 12:39:37 +0100

    Prevent errors on tasks app

--- apps/tasks/laboite/apps/tasks/forms.py
+++ apps/tasks/laboite/apps/tasks/forms.py
@@ -1,4 +1,8 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
 from django import forms
+from django.utils.translation import ugettext as _
 
 from .models import AppTasks, get_projects
 
@@ -14,16 +18,24 @@ class ProjectIdWidget(forms.Select):
         try:
             self.choices = [(proj['id'], proj['name']) for proj in get_projects(asana_personal_access_token)]
             self.enable()
+            return True
         except:
             self.disable()
+            return False
 
 
 class TasksModelForm(forms.ModelForm):
     def __init__(self, *args, **kw):
         super(TasksModelForm, self).__init__(*args, **kw)
         widget = self.fields['asana_project_id'].widget
-        if self.instance is not None and self.instance.asana_personal_access_token:
-            widget.load_project_ids(self.instance.asana_personal_access_token)
+        value = ''
+        if 'asana_personal_access_token' in self.data and self.data['asana_personal_access_token']:
+            value = self.data['asana_personal_access_token']
+        elif self.instance is not None and self.instance.asana_personal_access_token:
+            value = self.instance.asana_personal_access_token
+        if value:
+            if not widget.load_project_ids(value):
+                self.errors['asana_personal_access_token'] = [_("Clé d'API invalide.")]
         else:
             widget.disable()
 
