f1d1ac7bd7d6ec099b45d790c3517b7a03a6b1b6
Rémy HUBSCHER <hubscher.remy@gmail.com>
2018-06-09 00:34:13 +0200

    Use unlimited URL size

--- laboite/apps/data/models.py
+++ laboite/apps/data/models.py
@@ -21,7 +21,7 @@ class AppData(App):
         (24*60, _('Tous les jours')),
     )
 
-    url = models.URLField(_('URL de la ressource JSON'), help_text=_("Veuillez indiquer l'adresse de la ressource que vous souhaitez atteindre"), default='http://api.tom.tools/hits', null=False, blank=False)
+    url = models.TextField(_('URL de la ressource JSON'), help_text=_("Veuillez indiquer l'adresse de la ressource que vous souhaitez atteindre"), default='http://api.tom.tools/hits', null=False, blank=False)
     json_path = models.CharField(_("Chemin de l'élément JSON (JSON Path)"), help_text=_("Veuillez indiquer le chemin de l'élément json (exemple: articles[0].title)"), max_length=64, null=False, blank=False)
     json = models.TextField(_("Dernière version de la ressource JSON"), null=True, blank=True)
     data = models.CharField(_("Chemin de l'élément JSON (JSON Path)"), max_length=32)
