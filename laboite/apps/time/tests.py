1e6fb68bd7bc58a91f1da076d1ca5e86b2cd7103
Vincent Rialland <vincent.rialland@gmail.com>
2020-07-12 14:57:44 +0200

    Update test requirements

--- laboite/apps/time/tests.py
+++ laboite/apps/time/tests.py
@@ -13,49 +13,48 @@ def test_all(boite, mocker):
 
     translation.activate('fr')
 
-    with mocker.patch('laboite.apps.time.models.AppTime.should_update',
-                      return_value=True):  # Force update
-        app = AppTime.objects.create(boite=boite,
-                                     enabled=True,
-                                     tz='Africa/Niamey')
-        result = app.get_app_dictionary()
-        assert len(result) == 3
-        assert result['data'] == [{'type': 'text',
-                                   'width': 25,
-                                   'height': 8,
-                                   'x': 4,
-                                   'y': 1,
-                                   'color': 2,
-                                   'font': 1,
-                                   'content': '14:35'}]
-        assert result['height'] == 8
-        assert result['width'] == 32
+    mocker.patch('laboite.apps.time.models.AppTime.should_update', return_value=True)  # Force update
+    app = AppTime.objects.create(boite=boite,
+                                 enabled=True,
+                                 tz='Africa/Niamey')
+    result = app.get_app_dictionary()
+    assert len(result) == 3
+    assert result['data'] == [{'type': 'text',
+                               'width': 25,
+                               'height': 8,
+                               'x': 4,
+                               'y': 1,
+                               'color': 2,
+                               'font': 1,
+                               'content': '14:35'}]
+    assert result['height'] == 8
+    assert result['width'] == 32
 
-        app.tz = 'Pacific/Pitcairn'
-        app.save()
-        result = app.get_app_dictionary()
-        assert len(result) == 3
-        assert result['data'] == [{'type': 'text',
-                                   'width': 25,
-                                   'height': 8,
-                                   'x': 4,
-                                   'y': 1,
-                                   'color': 2,
-                                   'font': 1,
-                                   'content': '05:35'}]
-        assert result['height'] == 8
-        assert result['width'] == 32
+    app.tz = 'Pacific/Pitcairn'
+    app.save()
+    result = app.get_app_dictionary()
+    assert len(result) == 3
+    assert result['data'] == [{'type': 'text',
+                               'width': 25,
+                               'height': 8,
+                               'x': 4,
+                               'y': 1,
+                               'color': 2,
+                               'font': 1,
+                               'content': '05:35'}]
+    assert result['height'] == 8
+    assert result['width'] == 32
 
-        translation.activate('en-us')
-        result = app.get_app_dictionary()
-        assert len(result) == 3
-        assert result['data'] == [{'type': 'text',
-                                   'width': 25,
-                                   'height': 8,
-                                   'x': 4,
-                                   'y': 1,
-                                   'color': 2,
-                                   'font': 1,
-                                   'content': '5:35 a.m.'}]
-        assert result['height'] == 8
-        assert result['width'] == 32
+    translation.activate('en-us')
+    result = app.get_app_dictionary()
+    assert len(result) == 3
+    assert result['data'] == [{'type': 'text',
+                               'width': 25,
+                               'height': 8,
+                               'x': 4,
+                               'y': 1,
+                               'color': 2,
+                               'font': 1,
+                               'content': '5:35 a.m.'}]
+    assert result['height'] == 8
+    assert result['width'] == 32
