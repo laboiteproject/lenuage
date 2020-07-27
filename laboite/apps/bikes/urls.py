7639bb5090186cef0d5e05ef0d2808d49618bae4
Rémy HUBSCHER <hubscher.remy@gmail.com>
2020-07-27 10:25:47 +0200

    Fix flake8

--- laboite/apps/bikes/urls.py
+++ laboite/apps/bikes/urls.py
@@ -7,8 +7,8 @@ app_name = "laboite"
 
 urlpatterns = [
     path("station-autocomplete/",
-        login_required(StationAutoComplete.as_view()),
-        name='station-autocomplete'),
+         login_required(StationAutoComplete.as_view()),
+         name='station-autocomplete'),
     path("create/", login_required(AppBikesCreateView.as_view()), name="create"),
     path("<int:pk>/", login_required(AppBikesUpdateView.as_view()), name="update"),
     path("<int:pk>/delete/", login_required(AppBikesDeleteView.as_view()), name="delete"),
